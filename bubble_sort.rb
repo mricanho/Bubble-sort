d58627e0cf9f80fce96813d96cf810a93bab79d5
Miguel Ricaño <mricanho@gmail.com>
2020-11-24 16:04:55 -0600

    Start sort algorithm

---
+++ bubble_sort.rb
@@ -0,0 +1,21 @@
+
+
+def bubble_sort(arr)
+
+
+  number_of_loops = arr.length - 1
+  swap = true
+
+  while swap
+
+    swap = false
+    number_of_loops.times do |x|
+      arr[x], arr[x + 1] = arr[x + 1], arr[x] if arr[x] > arr[x + 1]
+      swap = true
+    end
+    number_of_loops -= 1
+  end
+  arr
+end
+
+puts bubble_sort([3, 2, 1, 85, 1, 58, 6, 4])
