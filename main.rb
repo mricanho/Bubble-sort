e3d84dd2f985ef5f094f610676b23b3037d66a95
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2020-11-25 22:51:19 +0300

    solve the second problem

--- main.rb
+++ main.rb
@@ -13,38 +13,36 @@
 # end
 # puts bubble_sort([3, 2, 1, 85, 1, 58, 6, 4])
 
-
-# Another way to slove it: 
-
+# Another way to slove it:
 def bubble_sort(arr)
-  number_ofloop = arr.length - 1 #loop inside the index 
-  for i in 0..number_ofloop  do # the first outside loop
-    for j in 0...(number_ofloop) do # the inside loop
-      if arr[j] > arr[j+1] 
-        arr[j] , arr[j+1] = arr[j+1] , arr[j] #replacement of the elements
-        end
+  number_ofloop = arr.length - 1 # loop inside the index
+  (0..number_ofloop).each do |_i| # the first outside loop
+    (0...number_ofloop).each do |j| # the inside loop
+      if arr[j] > arr[j + 1]
+        arr[j], arr[j + 1] = arr[j + 1], arr[j] # replacement of the elements
+      end
     end
   end
- arr
+  arr
 end
 
-puts bubble_sort([3, 2, 1, 85, 1, 58, 6, 4])
+puts bubble_sort([4, 3, 78, 2, 0, 2])
 
+# The second:
 
-# The second one:
 def bubble_sort_by(arr)
-  number_ofloop = arr.length - 1 #loop inside the index not the length
-  for i in 0..number_ofloop  do
-      for j in 0...(number_ofloop) do
-          if arr[j].length > arr[j+1].length #compare between lengths of the texts
-             arr[j] , arr[j+1] = arr[j+1] , arr[j]
-          end
+  number_ofloop = arr.length - 1 # loop inside the index not the length
+  (0..number_ofloop).each do |_i|
+    (0...number_ofloop).each do |j|
+      if arr[j].length > arr[j + 1].length # compare between lengths of the texts
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
       end
+    end
   end
-arr 
+  arr
 end
 
-compare = bubble_sort_by(["hi", "hello","hey"]) do |left,right|
+compare = bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
 puts compare
