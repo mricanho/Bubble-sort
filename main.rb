3a3fe678a37c40979db2abd2eff704fce69f6d5a
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2020-11-26 00:45:42 +0300

    fix review comments

--- main.rb
+++ main.rb
@@ -1,27 +1,12 @@
-# def bubble_sort(arr)
-#   number_of_loops = arr.length - 1
-#   swap = true
-#   while swap
-#     swap = false
-#     number_of_loops.times do |x|
-#       arr[x], arr[x + 1] = arr[x + 1], arr[x] if arr[x] > arr[x + 1]
-#       swap = true
-#     end
-#     number_of_loops -= 1
-#   end
-#   arr
-# end
-# puts bubble_sort([3, 2, 1, 85, 1, 58, 6, 4])
 
-# Another way to solve it:
 def bubble_sort(arr)
-  number_ofloop = arr.length - 1 # loop inside the index
-  (0..number_ofloop).each do |_i| # the first outside loop
-    (0...number_ofloop).each do |j| # the inside loop
-      arr[j], arr[j + 1] = arr[j + 1], arr[j] if arr[j] > arr[j + 1] # replacement of the elements
-    end
+  number_ofloop = arr.length - 1 
+  for i in 0..number_ofloop  do
+   for j in 0...(number_ofloop) do
+    arr[j] , arr[j+1] = arr[j+1] , arr[j] if arr[j] > arr[j+1] 
+   end
   end
-  arr
+ arr
 end
 
 puts bubble_sort([4, 3, 78, 2, 0, 2])
@@ -29,16 +14,13 @@ puts bubble_sort([4, 3, 78, 2, 0, 2])
 # The second:
 
 def bubble_sort_by(arr)
-  number_ofloop = arr.length - 1 # loop inside the index not the length
-  (0..number_ofloop).each do |_i|
-    (0...number_ofloop).each do |j|
-      arr[j], arr[j + 1] = arr[j + 1], arr[j] if arr[j].length > arr[j + 1].length
+  number_ofloop = arr.length - 1 #loop inside the index not the length
+  for i in 0..number_ofloop  do
+    for j in 0...(number_ofloop) do
+      if yield(arr[j] , arr[j+1]).positive?
+        arr[j] , arr[j+1] = arr[j+1] , arr[j] 
+      end
     end
   end
-  arr
+arr 
 end
-
-compare = bubble_sort_by(%w[hi hello hey]) do |left, right|
-  left.length - right.length
-end
-puts compare
