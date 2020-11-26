6a108a81b5af53d1d231ec616409575ef1c74246
Miguel Ricaño <mricanho@gmail.com>
2020-11-25 23:14:16 -0600

    Fix syntax, final review

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 def bubble_sort(arr)
   number_ofloop = arr.length - 1
-  (0..number_ofloop).each do |_i|
+  (0..number_ofloop).each do
     (0...number_ofloop).each do |j|
       arr[j], arr[j + 1] = arr[j + 1], arr[j] if arr[j] > arr[j + 1]
     end
@@ -8,13 +8,11 @@ def bubble_sort(arr)
   arr
 end
 
-puts bubble_sort([4, 3, 78, 2, 0, 2])
-
 # The second:
 
 def bubble_sort_by(arr)
   number_ofloop = arr.length - 1 # loop inside the index not the length
-  (0..number_ofloop).each do |_i|
+  (0..number_ofloop).each do
     (0...number_ofloop).each do |j|
       arr[j], arr[j + 1] = arr[j + 1], arr[j] if yield(arr[j], arr[j + 1]).positive?
     end
